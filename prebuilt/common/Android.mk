63c8a54e4806d6225ceef2cbab467f9f1026dfbe
Jon West <electrikjesus@gmail.com>
2020-11-23 16:52:03 -0500

    Remove wget
    We get it from Busybox
    Change-Id: Iba10ab7c59dce5173728d82e2645356f2c615ba3

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -1,15 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# a wrapper for curl which provides wget syntax, for compatibility
-include $(CLEAR_VARS)
-LOCAL_MODULE := wget
-LOCAL_SRC_FILES := bin/wget
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := fonts_customization.xml
 LOCAL_SRC_FILES := etc/fonts_customization.xml
