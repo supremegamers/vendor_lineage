b908178e0791b181507376f5075b66fcbe6aa9a4
DeinMathelehrer <58335765+DeinMathelehrer@users.noreply.github.com>
2021-07-18 01:46:21 +0200

    vendor: add blur flag
    TARGET_USES_BLUR is the blur flag for now, it is normally on true but can be turned to false if device is too slow or has other problems

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,6 +7,13 @@ PRODUCT_SIZE := full
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
+# Blur
+ifeq ($(TARGET_USES_BLUR),true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
