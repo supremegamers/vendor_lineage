ddd290456df0da0117e4923447eecf6ed31aa38a
Beru Shinsetsu <windowz414@gnuweeb.org>
2021-12-19 23:52:45 +0000

    build/tasks/bandori: Initial system for signing builds
    * TODO: Unify the if-else statement inside a separate makefile or
      shell script and call it instead.
    
    Signed-off-by: Beru Shinsetsu <windowz414@gnuweeb.org>

--- build/tasks/bandori.mk
+++ build/tasks/bandori.mk
@@ -1,5 +1,6 @@
 # Copyright (C) 2017 Unlegacy-Android
 # Copyright (C) 2017,2020 The LineageOS Project
+# Copyright (C) 2021,2022 Beru Shinsetsu (On behalf of Project Kasumi)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,14 +22,34 @@ LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/Kasumi-$(LINEAGE_VERSION).zip
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bandori
-bandori: $(INTERNAL_OTA_PACKAGE_TARGET)
+ifeq ($(LINEAGE_BUILDTYPE),OFFICIAL)
+# This build is marked as official and requires signing.
+# TODO: Unify the process inside a separate makefile/shell script and call it instead.
+bandori: $(INTERNAL_OTA_PACKAGE_TARGET) otatools target-files-package
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
+	$(hide) mv $(LINEAGE_TARGET_PACKAGE) $(LINEAGE_TARGET_PACKAGE).unsigned
+	sign_target_files_apks -o -d vendor/priv $(PRODUCT_OUT)/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $(PRODUCT_OUT)/signed-target_files.zip >&2
+	ota_from_target_files -k vendor/priv/releasekey --block --backup=true $(PRODUCT_OUT)/signed-target_files.zip $(LINEAGE_TARGET_PACKAGE) >&2
 	$(hide) $(MD5) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	@echo "//          Project Kasumi          //" >&2
 	@echo "// PoPiPa, PiPoPa, PoPiPaPaPiPoPa~! //" >&2
 	@echo "" >&2
-	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
 	@echo "" >&2
 	@echo "To get started, get your custom recovery up and slap this ROM in!" >&2
 	@echo "Based on Project Materium, brought to you by Beru Shinsetsu (@WindowZ414) feat. Yuki (@cyberyuki)." >&2
+else
+# Builds that aren't marked as official aren't required to be signed.
+bandori: $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
+	$(hide) $(MD5) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
+	@echo "//          Project Kasumi          //" >&2
+	@echo "// PoPiPa, PiPoPa, PoPiPaPaPiPoPa~! //" >&2
+	@echo "" >&2
+	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+	@echo "" >&2
+	@echo "To get started, get your custom recovery up and slap this ROM in!" >&2
+	@echo "Based on Project Materium, brought to you by Beru Shinsetsu (@WindowZ414) feat. Yuki (@cyberyuki)." >&2
+endif
