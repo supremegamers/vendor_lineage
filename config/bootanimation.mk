2817c4ec324d5259c5e4f4cca56e528aab6ccb7b
Beru Shinsetsu <windowz414@1337.lgbt>
2022-02-16 06:53:14 +0000

    config/bootanimation: Use `strip` instead of `sed` to filter space
    * This will remove whitespaces before and after the value we need.
    
    Signed-off-by: Beru Shinsetsu <windowz414@1337.lgbt>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -22,10 +22,10 @@ ifeq ($(TARGET_CUSTOM_BOOT_ANIMATION),)
 
      # We don't know if the maintainer added space after definition in tree.
      # (E.g. "TARGET_BOOT_ANIMATION_RES := 720 # Random thing)
-     # This causes COPY_FILES to malfunction, so delete everything after
-     # first space out. There shouldn't be a space before the actual value
-     # anyway, Rest In Pepperoni if there is. xD
-     TARGET_BOOT_ANIMATION_RES := $(shell echo $(TARGET_BOOT_ANIMATION_RES) | sed -e 's/ .*//')
+     # This causes COPY_FILES to malfunction, so trim it. Everything after
+     # pound sign is already filtered out by build system so nothing should
+     # go wrong.
+     TARGET_BOOT_ANIMATION_RES := $(strip $(TARGET_BOOT_ANIMATION_RES))
      TARGET_CUSTOM_BOOT_ANIMATION := vendor/kasumi/prebuilt/common/media/bootanimation-$(TARGET_BOOT_ANIMATION_RES).zip
 endif
 
